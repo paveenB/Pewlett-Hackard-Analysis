1a909db4b37f32f969588e51b2c506a96ca42c58
Win Bunyasrie <paveen.bunyasrie@gmail.com>
2022-02-05 17:52:24 -0500

    Updating Schema.sql file with corrections

--- schema.sql
+++ schema.sql
@@ -16,7 +16,7 @@ CREATE TABLE employees (
 	PRIMARY KEY (emp_no)
 );
 
-CREATE TABLE debt_manager (
+CREATE TABLE dept_manager (
 	dept_no VARCHAR(4) NOT NULL,
 	emp_no INT NOT NULL,
 	from_date DATE NOT NULL,
@@ -37,10 +37,11 @@ CREATE TABLE salaries (
 
 CREATE TABLE dept_emp (
 	emp_no INT NOT NULL,
-	debt_no INT NOT NULL,
+	dept_no INT NOT NULL,
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
 	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
 	PRIMARY KEY (emp_no)
 );
 
