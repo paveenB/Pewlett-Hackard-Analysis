94b6a1af20ba8d9952cc0afedeeeeb681274f333
Win Bunyasrie <paveen.bunyasrie@gmail.com>
2022-02-20 16:41:08 -0500

    Updating Queries.sql file

--- Queries/Queries.sql
+++ Queries/Queries.sql
@@ -136,10 +136,6 @@ WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
 AND (e.hire_date BETWEEN '1985-01-01' AND '1988-12-31')
 AND (de.to_date = '9999-01-01');
 
-
-
-
-
 -- List of managers per department
 SELECT  dm.dept_no,
         d.dept_name,
